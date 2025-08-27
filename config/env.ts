2311b2320e4c16064c89009ea410ba449540dd99
moonstar-x <christian.lopez99@outlook.com>
2025-08-27 00:07:45 -0500

    Added dummy values to avoid testing from breaking.,

--- config/env.ts
+++ config/env.ts
@@ -8,6 +8,7 @@ export const KIPU_BANK_MAX_SINGLE_WITHDRAW_LIMIT = process.env.KIPU_BANK_MAX_SIN
   parseInt(process.env.KIPU_BANK_MAX_SINGLE_WITHDRAW_LIMIT, 10) :
   undefined;
 
-export const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL!;
-export const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY!;
-export const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY!;
+// Note: Default values are dummy values to satisfy Hardhat so tests can run.
+export const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL ?? 'https://eth-sepolia.g.alchemy.com';
+export const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY ?? '27fcf96996e466a3df40ce620740902eaf92fc606627c79bf4a68dfb7abe6d1e';
+export const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY ?? 'ETHERSCAN_API_KEY';

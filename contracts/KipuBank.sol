468cb88354bba49ebeed05899edfc047aaef989b
moonstar-x <christian.lopez99@outlook.com>
2025-10-02 23:55:32 -0500

    Fixed linting and formatting errors.

--- contracts/KipuBank.sol
+++ contracts/KipuBank.sol
@@ -5,206 +5,267 @@ pragma solidity ^0.8.28;
 import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 /**
+ * @title Kipu Bank Interface
+ * @author Christian López (@moonstar-x)
+ * @notice The interface that KipuBank must implement.
+ * @dev Used to document which functions are expected to be available in the contract.
+ */
+interface IKipuBank {
+    /**
+     * @notice Allows contract to receive ETH directly.
+     */
+    receive() external payable;
+
+    /**
+     * @notice Deposits the value in the address' vault.
+     */
+    function deposit() external payable;
+
+    /**
+     * @notice Withdraws amount from the address' vault.
+     * @param _amount The amount to withdraw.
+     */
+    function withdraw(uint256 _amount) external;
+
+    /**
+     * @notice Get balance in this contract.
+     * @return balance_ The balance of this contract.
+     */
+    function getBalance() external view returns (uint256 balance_);
+
+    /**
+     * @notice Get balance for the sender.
+     * @return funds_ The balance of the sender.
+     */
+    function getMyFunds() external view returns (uint256 funds_);
+
+    /**
+     * @notice Get funds stored in vault for a given address.
+     * @param _address The address to check the funds for.
+     * @return funds_ The funds stored in vault for the given address.
+     */
+    function getFundsForAddress(address _address) external view returns (uint256 funds_);
+
+    /**
+     * @notice Get this contract's deposit count.
+     * @return depositCount_ The deposit count.
+     */
+    function getDepositCount() external view returns (uint256 depositCount_);
+
+    /**
+     * @notice Get this contract's withdraw count.
+     * @return withdrawCount_ The withdraw count.
+     */
+    function getWithdrawCount() external view returns (uint256 withdrawCount_);
+}
+
+/**
  * @title Kipu Bank
  * @author Christian López (@moonstar-x)
  * @notice A smart contract for secure ETH deposits and withdrawals with bank cap and single withdrawal limit.
  * @dev Do not use in production. This is an educational example.
  */
-contract KipuBank is ReentrancyGuard {
-  /**
-   * @notice The address used to represent ETH in this contract.
-   */
-  address constant ETH_ADDRESS = address(0);
+contract KipuBank is IKipuBank, ReentrancyGuard {
+    /**
+     * @notice The address used to represent ETH in this contract.
+     */
+    address internal constant ETH_ADDRESS = address(0);
 
-  /**
-   * @notice The maximum value that this contract can hold.
-   */
-  uint256 private immutable i_bankCap;
+    /**
+     * @notice The maximum value that this contract can hold.
+     */
+    uint256 private immutable i_bankCap;
 
-  /**
-   * @notice The maximum amount that a user can withdraw from their vault in a single transaction.
-   */
-  uint256 private immutable i_maxSingleWithdrawLimit;
+    /**
+     * @notice The maximum amount that a user can withdraw from their vault in a single transaction.
+     */
+    uint256 private immutable i_maxSingleWithdrawLimit;
 
-  /**
-   * @notice Counter for each successful deposit.
-   */
-  uint256 private s_depositCount = 0;
+    /**
+     * @notice Counter for each successful deposit.
+     */
+    uint256 private s_depositCount = 0;
 
-  /**
-   * @notice Counter for each successful withdrawal.
-   */
-  uint256 private s_withdrawCount = 0;
+    /**
+     * @notice Counter for each successful withdrawal.
+     */
+    uint256 private s_withdrawCount = 0;
 
-  // TODO: This should store multiple tokens.
-  /**
-   * @notice Vault that keeps funds per address.
-   */
-  mapping(address => uint256) private s_vault;
+    // TODO: This should store multiple tokens.
+    /**
+     * @notice Vault that keeps funds per address.
+     */
+    mapping(address _userAddress => uint256 _amount) private s_vault;
 
-  /**
-   * @notice Event emitted when a deposit is successful.
-   * @param _address The address of the message sender.
-   * @param _amount The amount deposited.
-   */
-  event DepositSuccess(address _address, uint256 _amount);
+    /**
+     * @notice Event emitted when a deposit is successful.
+     * @param _address The address of the message sender.
+     * @param _amount The amount deposited.
+     */
+    event DepositSuccess(address indexed _address, uint256 indexed _amount);
 
-  /**
-   * @notice Event emitted when a withdraw is successful.
-   * @param _address The address of the message sender.
-   * @param _amount The amount withdrawn.
-   */
-  event WithdrawSuccess(address _address, uint256 _amount);
+    /**
+     * @notice Event emitted when a withdraw is successful.
+     * @param _address The address of the message sender.
+     * @param _amount The amount withdrawn.
+     */
+    event WithdrawSuccess(address indexed _address, uint256 indexed _amount);
 
-  /**
-   * @notice Error thrown when the constructor preconditions are not met.
-   * @param _reason The reason why the precondition failed.
-   */
-  error ConstructorPreconditionError(string _reason);
+    /**
+     * @notice Error thrown when the constructor preconditions are not met.
+     * @param _reason The reason why the precondition failed.
+     */
+    error ConstructorPreconditionError(string _reason);
 
-  /**
-   * @notice Error thrown when the contract has or would reach the bank cap set in the deployment step by the current deposit.
-   */
-  error BankCapReachedError();
+    /**
+     * @notice Error thrown when the contract has or would reach the bank cap set in
+     * the deployment step by the current deposit.
+     */
+    error BankCapReachedError();
 
-  /**
-   * @notice Error thrown when the current withdrawal request would exceed the withdraw limit set in the deployment step.
-   * @param _sender The address of the sender.
-   * @param _amount The amount that was attempted to be withdrawn.
-   */
-  error WithdrawLimitExceededError(address _sender, uint256 _amount);
+    /**
+     * @notice Error thrown when the current withdrawal request would exceed the withdraw
+     * limit set in the deployment step.
+     * @param _sender The address of the sender.
+     * @param _amount The amount that was attempted to be withdrawn.
+     */
+    error WithdrawLimitExceededError(address _sender, uint256 _amount);
 
-  /**
-   * @notice Error thrown when a withdrawal request attempts to withdraw an amount that exceeds the funds stored in the contract.
-   * @param _sender The address of the sender.
-   * @param _funds The funds in the sender's vault.
-   * @param _amount The amount that was attempted to be withdrawn.
-   */
-  error InsufficientFundsError(address _sender, uint256 _funds, uint256 _amount);
+    /**
+     * @notice Error thrown when a withdrawal request attempts to withdraw an amount that
+     * exceeds the funds stored in the contract.
+     * @param _sender The address of the sender.
+     * @param _funds The funds in the sender's vault.
+     * @param _amount The amount that was attempted to be withdrawn.
+     */
+    error InsufficientFundsError(address _sender, uint256 _funds, uint256 _amount);
 
-  /**
-   * @notice Error thrown if a transfer was not successful.
-   */
-  error TransferError();
+    /**
+     * @notice Error thrown if a transfer was not successful.
+     */
+    error TransferError();
 
-  /**
-   * @notice Deploys the contract by setting the bank cap and the maximum single withdrawal limit.
-   * @param _bankCap The maximum value that this contract can hold.
-   * @param _maxWithdrawLimit The maximum amount that a user can withdraw from their vault in a single transaction.
-   */
-  constructor(uint256 _bankCap, uint256 _maxWithdrawLimit) {
-    if (_bankCap < _maxWithdrawLimit) {
-      revert ConstructorPreconditionError("Bank cap must be greater than max withdraw limit.");
+    /**
+     * @notice Deploys the contract by setting the bank cap and the maximum single withdrawal limit.
+     * @param _bankCap The maximum value that this contract can hold.
+     * @param _maxWithdrawLimit The maximum amount that a user can withdraw from their vault in a single transaction.
+     */
+    constructor(uint256 _bankCap, uint256 _maxWithdrawLimit) {
+        if (_bankCap < _maxWithdrawLimit) {
+            revert ConstructorPreconditionError("Exp. bankCap >= maxWithdrawLimit");
+        }
+
+        i_bankCap = _bankCap;
+        i_maxSingleWithdrawLimit = _maxWithdrawLimit;
     }
 
-    i_bankCap = _bankCap;
-    i_maxSingleWithdrawLimit = _maxWithdrawLimit;
-  }
-
-  /**
-   * @notice Deposits the value in the address' vault.
-   */
-  function deposit() public payable {
-    uint256 potentialBankValue = getBalance() + msg.value;
-    if (potentialBankValue > i_bankCap) {
-      revert BankCapReachedError();
+    /**
+     * @notice Allows contract to receive ETH directly.
+     */
+    receive() external payable override {
+        deposit();
     }
 
-    _updateDepositValues(msg.sender, msg.value);
-    emit DepositSuccess(msg.sender, msg.value);
-  }
-
-  // TODO: Implement deposit of other tokens.
-
-  /**
-   * @notice Withdraws amount from the address' vault.
-   * @param _amount The amount to withdraw.
-   */
-  function withdraw(uint256 _amount) public nonReentrant {
-    if (_amount > i_maxSingleWithdrawLimit) {
-      revert WithdrawLimitExceededError(msg.sender, i_maxSingleWithdrawLimit);
+    /**
+     * @notice Get balance for the sender.
+     * @return funds_ The balance of the sender.
+     */
+    function getMyFunds() external view override returns (uint256 funds_) {
+        funds_ = s_vault[msg.sender];
     }
 
-    uint256 funds = s_vault[msg.sender];
-    if (_amount > funds) {
-      revert InsufficientFundsError(msg.sender, funds, _amount);
+    /**
+     * @notice Get funds stored in vault for a given address.
+     * @param _address The address to check the funds for.
+     * @return funds_ The funds stored in vault for the given address.
+     */
+    function getFundsForAddress(address _address) external view override returns (uint256 funds_) {
+        funds_ = s_vault[_address];
     }
 
-    _updateWithdrawValues(msg.sender, _amount);
-    emit WithdrawSuccess(msg.sender, _amount);
-
-    address payable payableSender = payable(msg.sender);
-    (bool success, ) = payableSender.call{ value: _amount }("");
-    if (!success) {
-      revert TransferError();
+    /**
+     * @notice Get this contract's deposit count.
+     * @return depositCount_ The deposit count.
+     */
+    function getDepositCount() external view override returns (uint256 depositCount_) {
+        depositCount_ = s_depositCount;
     }
-  }
 
-  // TODO: Implement conversion between ETH and USD.
-  // TODO: Implement chainlink oracle price feed.
+    /**
+     * @notice Get this contract's withdraw count.
+     * @return withdrawCount_ The withdraw count.
+     */
+    function getWithdrawCount() external view override returns (uint256 withdrawCount_) {
+        withdrawCount_ = s_withdrawCount;
+    }
 
-  /**
-   * @notice Get balance in this contract.
-   * @return The balance of this contract.
-   */
-  function getBalance() public view returns (uint256) {
-    return address(this).balance;
-  }
+    /**
+     * @notice Deposits the value in the address' vault.
+     */
+    function deposit() public payable override {
+        uint256 potentialBankValue = getBalance() + msg.value;
+        if (potentialBankValue > i_bankCap) {
+            revert BankCapReachedError();
+        }
 
+        _updateDepositValues(msg.sender, msg.value);
+        emit DepositSuccess(msg.sender, msg.value);
+    }
 
-  /**
-   * @notice Get balance for the sender.
-   * @return The balance of the sender.
-   */
-  function getMyFunds() external view returns (uint256) {
-    return s_vault[msg.sender];
-  }
+    // TODO: Implement deposit of other tokens.
 
-  /**
-   * @notice Get funds stored in vault for a given address.
-   * @param _address The address to check the funds for.
-   * @return The funds stored in vault for the given address.
-   */
-  function getFundsForAddress(address _address) external view returns (uint256) {
-    return s_vault[_address];
-  }
+    /**
+     * @notice Withdraws amount from the address' vault.
+     * @param _amount The amount to withdraw.
+     */
+    function withdraw(uint256 _amount) public override nonReentrant {
+        if (_amount > i_maxSingleWithdrawLimit) {
+            revert WithdrawLimitExceededError(msg.sender, i_maxSingleWithdrawLimit);
+        }
 
-  /**
-   * @notice Get this contract's deposit count.
-   * @return The deposit count.
-   */
-  function getDepositCount() external view returns (uint256) {
-    return s_depositCount;
-  }
+        uint256 funds = s_vault[msg.sender];
+        if (_amount > funds) {
+            revert InsufficientFundsError(msg.sender, funds, _amount);
+        }
 
-  /**
-   * @notice Get this contract's withdraw count.
-   * @return The withdraw count.
-   */
-  function getWithdrawCount() external view returns (uint256) {
-    return s_withdrawCount;
-  }
+        _updateWithdrawValues(msg.sender, _amount);
+        emit WithdrawSuccess(msg.sender, _amount);
 
-  /**
-   * @notice Update the relevant variables when a deposit is successful.
-   */
-  function _updateDepositValues(address _address, uint256 _amount) private {
-    s_vault[_address] += _amount;
-    s_depositCount++;
-  }
+        address payable payableSender = payable(msg.sender);
+        (bool success, ) = payableSender.call{ value: _amount }("");
+        if (!success) {
+            revert TransferError();
+        }
+    }
 
-  /**
-   * @notice Update the relevant variables when a withdrawal is successful.
-   */
-  function _updateWithdrawValues(address _address, uint256 _amount) private {
-    s_vault[_address] -= _amount;
-    s_withdrawCount++;
-  }
+    // TODO: Implement conversion between ETH and USD.
+    // TODO: Implement chainlink oracle price feed.
 
-  /**
-   * @notice Allows contract to receive ETH directly.
-   */
-  receive() external payable {
-    deposit();
-  }
+    /**
+     * @notice Get balance in this contract.
+     * @return balance_ The balance of this contract.
+     */
+    function getBalance() public view override returns (uint256 balance_) {
+        balance_ = address(this).balance;
+    }
+
+    /**
+     * @notice Update the relevant variables when a deposit is successful.
+     * @param _address The address of the user making the deposit.
+     * @param _amount The amount deposited.
+     */
+    function _updateDepositValues(address _address, uint256 _amount) private {
+        s_vault[_address] += _amount;
+        ++s_depositCount;
+    }
+
+    /**
+     * @notice Update the relevant variables when a withdrawal is successful.
+     * @param _address The address of the user making the withdrawal.
+     * @param _amount The amount withdrawn.
+     */
+    function _updateWithdrawValues(address _address, uint256 _amount) private {
+        s_vault[_address] -= _amount;
+        ++s_withdrawCount;
+    }
 }
--- contracts/KipuBank.t.sol
+++ contracts/KipuBank.t.sol
@@ -5,103 +5,101 @@ import { KipuBank } from "./KipuBank.sol";
 import { Test } from "forge-std/Test.sol";
 
 contract KipuBankTest is Test {
-  uint256 private constant BANK_CAP = 300;
-  uint256 private constant MAX_SINGLE_WITHDRAW_LIMIT = 100;
+    uint256 private constant BANK_CAP = 300;
+    uint256 private constant MAX_SINGLE_WITHDRAW_LIMIT = 100;
 
-  KipuBank bank;
+    KipuBank bank;
 
-  function setUp() public {
-    bank = new KipuBank(BANK_CAP, MAX_SINGLE_WITHDRAW_LIMIT);
-  }
+    receive() external payable {}
 
-  function test_ConstructorShouldRevertIfPreconditionsNotMet() public {
-    vm.expectPartialRevert(KipuBank.ConstructorPreconditionError.selector);
-    bank = new KipuBank(50, 100);
-  }
+    function setUp() public {
+        bank = new KipuBank(BANK_CAP, MAX_SINGLE_WITHDRAW_LIMIT);
+    }
 
-  function test_DepositShouldRevertIfBankCapReached() public {
-    vm.expectRevert(KipuBank.BankCapReachedError.selector);
-    bank.deposit{ value: BANK_CAP + 1 }();
-  }
+    function test_ConstructorShouldRevertIfPreconditionsNotMet() public {
+        vm.expectPartialRevert(KipuBank.ConstructorPreconditionError.selector);
+        bank = new KipuBank(50, 100);
+    }
 
-  function test_DepositShouldUpdateValues() public {
-    address addr = address(this);
-    uint amount = 25;
+    function test_DepositShouldRevertIfBankCapReached() public {
+        vm.expectRevert(KipuBank.BankCapReachedError.selector);
+        bank.deposit{ value: BANK_CAP + 1 }();
+    }
 
-    vm.assertEq(bank.getFundsForAddress(addr), 0, "Funds should start at 0.");
-    vm.assertEq(bank.getBalance(), 0, "Bank should start at 0.");
-    vm.assertEq(bank.getDepositCount(), 0, "Deposit count should start at 0.");
+    function test_DepositShouldUpdateValues() public {
+        address addr = address(this);
+        uint amount = 25;
 
-    bank.deposit{ value: amount }();
+        vm.assertEq(bank.getFundsForAddress(addr), 0, "Funds should start at 0.");
+        vm.assertEq(bank.getBalance(), 0, "Bank should start at 0.");
+        vm.assertEq(bank.getDepositCount(), 0, "Deposit count should start at 0.");
 
-    vm.assertEq(bank.getFundsForAddress(addr), amount, "Funds should be updated.");
-    vm.assertEq(bank.getBalance(), amount, "Bank should be updated.");
-    vm.assertEq(bank.getDepositCount(), 1, "Deposit count be at 1.");
-  }
+        bank.deposit{ value: amount }();
 
-  function test_DepositShouldEmitSuccess() public {
-    vm.expectEmit();
-    emit KipuBank.DepositSuccess(address(this), 1);
+        vm.assertEq(bank.getFundsForAddress(addr), amount, "Funds should be updated.");
+        vm.assertEq(bank.getBalance(), amount, "Bank should be updated.");
+        vm.assertEq(bank.getDepositCount(), 1, "Deposit count be at 1.");
+    }
 
-    bank.deposit{ value: 1 }();
-  }
+    function test_DepositShouldEmitSuccess() public {
+        vm.expectEmit();
+        emit KipuBank.DepositSuccess(address(this), 1);
 
-  function test_WithdrawShouldRevertIfAmountExceedsLimit() public {
-    vm.expectPartialRevert(KipuBank.WithdrawLimitExceededError.selector);
-    bank.withdraw(MAX_SINGLE_WITHDRAW_LIMIT + 1);
-  }
+        bank.deposit{ value: 1 }();
+    }
 
-  function test_WithdrawShouldRevertIfAmountExceedsFunds() public {
-    vm.expectPartialRevert(KipuBank.InsufficientFundsError.selector);
-    bank.withdraw(10);
-  }
+    function test_WithdrawShouldRevertIfAmountExceedsLimit() public {
+        vm.expectPartialRevert(KipuBank.WithdrawLimitExceededError.selector);
+        bank.withdraw(MAX_SINGLE_WITHDRAW_LIMIT + 1);
+    }
 
-  function test_WithdrawShouldTransferAmountToSender() public {
-    vm.expectCall(address(bank), abi.encodeCall(bank.withdraw, (5)));
-    bank.deposit{ value: 10 }();
-    bank.withdraw(5);
-  }
+    function test_WithdrawShouldRevertIfAmountExceedsFunds() public {
+        vm.expectPartialRevert(KipuBank.InsufficientFundsError.selector);
+        bank.withdraw(10);
+    }
 
-  function test_WithdrawShouldUpdateValues() public {
-    address addr = address(this);
-    uint initialAmount = 25;
-    uint withdrawnAmount = 10;
+    function test_WithdrawShouldTransferAmountToSender() public {
+        vm.expectCall(address(bank), abi.encodeCall(bank.withdraw, (5)));
+        bank.deposit{ value: 10 }();
+        bank.withdraw(5);
+    }
 
-    vm.expectCall(address(bank), abi.encodeCall(bank.withdraw, (withdrawnAmount)));
+    function test_WithdrawShouldUpdateValues() public {
+        address addr = address(this);
+        uint initialAmount = 25;
+        uint withdrawnAmount = 10;
 
-    bank.deposit{ value: initialAmount }();
+        vm.expectCall(address(bank), abi.encodeCall(bank.withdraw, (withdrawnAmount)));
 
-    vm.assertEq(bank.getFundsForAddress(addr), initialAmount, "Funds should start at initial amount.");
-    vm.assertEq(bank.getBalance(), initialAmount, "Bank should start at initial amount.");
-    vm.assertEq(bank.getWithdrawCount(), 0, "Withdraw count should start at 0.");
+        bank.deposit{ value: initialAmount }();
 
-    bank.withdraw(withdrawnAmount);
+        vm.assertEq(bank.getFundsForAddress(addr), initialAmount, "Funds should start at initial amount.");
+        vm.assertEq(bank.getBalance(), initialAmount, "Bank should start at initial amount.");
+        vm.assertEq(bank.getWithdrawCount(), 0, "Withdraw count should start at 0.");
 
-    vm.assertEq(bank.getFundsForAddress(addr), initialAmount - withdrawnAmount, "Funds should be updated.");
-    vm.assertEq(bank.getBalance(), initialAmount - withdrawnAmount, "Bank should be updated.");
-    vm.assertEq(bank.getWithdrawCount(), 1, "Withdraw count should be at 1.");
-  }
+        bank.withdraw(withdrawnAmount);
 
-  function test_WithdrawShouldEmitSuccess() public {
-    bank.deposit{ value: 10 }();
+        vm.assertEq(bank.getFundsForAddress(addr), initialAmount - withdrawnAmount, "Funds should be updated.");
+        vm.assertEq(bank.getBalance(), initialAmount - withdrawnAmount, "Bank should be updated.");
+        vm.assertEq(bank.getWithdrawCount(), 1, "Withdraw count should be at 1.");
+    }
 
-    vm.expectEmit();
-    emit KipuBank.WithdrawSuccess(address(this), 5);
+    function test_WithdrawShouldEmitSuccess() public {
+        bank.deposit{ value: 10 }();
 
-    bank.withdraw(5);
-  }
+        vm.expectEmit();
+        emit KipuBank.WithdrawSuccess(address(this), 5);
 
-  function test_GetMyFundsShouldReturnUserFunds() public {
-    vm.assertEq(bank.getMyFunds(), 0, "My funds should initialized at 0.");
+        bank.withdraw(5);
+    }
 
-    bank.deposit{ value: 10 }();
-    vm.assertEq(bank.getMyFunds(), 10, "My funds should be updated after deposit.");
+    function test_GetMyFundsShouldReturnUserFunds() public {
+        vm.assertEq(bank.getMyFunds(), 0, "My funds should initialized at 0.");
 
-    bank.withdraw(5);
-    vm.assertEq(bank.getMyFunds(), 5, "My funds should be updated after withdraw.");
-  }
+        bank.deposit{ value: 10 }();
+        vm.assertEq(bank.getMyFunds(), 10, "My funds should be updated after deposit.");
 
-  receive() external payable {
-
-  }
+        bank.withdraw(5);
+        vm.assertEq(bank.getMyFunds(), 5, "My funds should be updated after withdraw.");
+    }
 }

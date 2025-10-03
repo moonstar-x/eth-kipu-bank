e7cd8a9612a42a367dbcc46e38d2bab709708bfd
moonstar-x <christian.lopez99@outlook.com>
2025-10-02 21:58:36 -0500

    Added missing natspec comments.

--- contracts/KipuBank.sol
+++ contracts/KipuBank.sol
@@ -3,71 +3,74 @@ pragma solidity ^0.8.28;
 
 import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
+/**
+ * @title Kipu Bank
+ * @author Christian López (@moonstar-x)
+ * @notice A smart contract for secure ETH deposits and withdrawals with bank cap and single withdrawal limit.
+ * @dev Do not use in production. This is an educational example.
+ */
 contract KipuBank is ReentrancyGuard {
   /**
-   * The maximum value tha this contract can hold.
+   * @notice The maximum value tha this contract can hold.
    */
   uint256 private immutable i_bankCap;
 
   /**
-   * The maximum amount that a user can withdraw from their vault in a single transaction.
+   * @notice The maximum amount that a user can withdraw from their vault in a single transaction.
    */
   uint256 private immutable i_maxSingleWithdrawLimit;
 
   /**
-   * Counter for each successful deposit.
+   * @notice Counter for each successful deposit.
    */
   uint256 private s_depositCount = 0;
 
   /**
-   * Counter for each successful withdrawal.
+   * @notice Counter for each successful withdrawal.
    */
   uint256 private s_withdrawCount = 0;
 
   /**
-   * Vault that keeps funds per address.
+   * @notice Vault that keeps funds per address.
    */
   mapping(address => uint256) private s_vault;
 
   /**
-   * Event emitted when a deposit is successful.
-   * @param addr The address of the message sender.
-   * @param amount The amount deposited.
+   * @notice Event emitted when a deposit is successful.
+   * @param _address The address of the message sender.
+   * @param _amount The amount deposited.
    */
-  event DepositSuccess(address addr, uint256 amount);
+  event DepositSuccess(address _address, uint256 _amount);
 
   /**
-   * Event emitted when a withdraw is successful.
-   * @param addr The address of the message sender.
-   * @param amount The amount withdrawn.
+   * @notice Event emitted when a withdraw is successful.
+   * @param _address The address of the message sender.
+   * @param _amount The amount withdrawn.
    */
-  event WithdrawSuccess(address addr, uint256 amount);
+  event WithdrawSuccess(address _address, uint256 _amount);
 
   /**
-   * Error thrown when the contract has or would reach the bank cap set
-   * in the deployment step by the current deposit.
+   * @notice Error thrown when the contract has or would reach the bank cap set in the deployment step by the current deposit.
    */
   error BankCapReachedError();
 
   /**
-   * Error thrown when the current withdrawal request would exceed the
-   * withdraw limit set in the deployment step.
-   * @param sender The address of the sender.
-   * @param amount The amount that was attempted to be withdrawn.
+   * @notice Error thrown when the current withdrawal request would exceed the withdraw limit set in the deployment step.
+   * @param _sender The address of the sender.
+   * @param _amount The amount that was attempted to be withdrawn.
    */
-  error WithdrawLimitExceededError(address sender, uint256 amount);
+  error WithdrawLimitExceededError(address _sender, uint256 _amount);
 
   /**
-   * Error thrown when a withdrawal request attempts to withdraw an amount
-   * that exceeds the funds stored in the contract.
-   * @param sender The address of the sender.
-   * @param funds The funds in the sender's vault.
-   * @param amount The amount that was attempted to be withdrawn.
+   * @notice Error thrown when a withdrawal request attempts to withdraw an amount that exceeds the funds stored in the contract.
+   * @param _sender The address of the sender.
+   * @param _funds The funds in the sender's vault.
+   * @param _amount The amount that was attempted to be withdrawn.
    */
-  error InsufficientFundsError(address sender, uint256 funds, uint256 amount);
+  error InsufficientFundsError(address _sender, uint256 _funds, uint256 _amount);
 
   /**
-   * Error thrown if a transfer was not successful.
+   * @notice Error thrown if a transfer was not successful.
    */
   error TransferError();
 
@@ -79,7 +82,7 @@ contract KipuBank is ReentrancyGuard {
   }
 
   /**
-   * Deposits the value in the address' vault.
+   * @notice Deposits the value in the address' vault.
    */
   function deposit() public payable {
     uint256 potentialBankValue = getBalance() + msg.value;
@@ -92,8 +95,8 @@ contract KipuBank is ReentrancyGuard {
   }
 
   /**
-   * Withdraws amount from the address' vault.
-   * @param _amount The amount to withdraw. 
+   * @notice Withdraws amount from the address' vault.
+   * @param _amount The amount to withdraw.
    */
   function withdraw(uint256 _amount) public nonReentrant {
     if (_amount > i_maxSingleWithdrawLimit) {
@@ -116,7 +119,7 @@ contract KipuBank is ReentrancyGuard {
   }
 
   /**
-   * Get balance in this contract.
+   * @notice Get balance in this contract.
    */
   function getBalance() public view returns (uint256) {
     return address(this).balance;
@@ -124,14 +127,14 @@ contract KipuBank is ReentrancyGuard {
 
 
   /**
-   * Get balance for the sender.
+   * @notice Get balance for the sender.
    */
   function getMyFunds() external view returns (uint256) {
     return s_vault[msg.sender];
   }
 
   /**
-   * Get funds stored in vault for a given address.
+   * @notice Get funds stored in vault for a given address.
    * @param _address The address to check the funds for.
    */
   function getFundsForAddress(address _address) external view returns (uint256) {
@@ -139,21 +142,21 @@ contract KipuBank is ReentrancyGuard {
   }
 
   /**
-   * Get this contract's deposit count.
+   * @notice Get this contract's deposit count.
    */
   function getDepositCount() external view returns (uint256) {
     return s_depositCount;
   }
 
   /**
-   * Get this contract's withdraw count.
+   * @notice Get this contract's withdraw count.
    */
   function getWithdrawCount() external view returns (uint256) {
     return s_withdrawCount;
   }
 
   /**
-   * Update the relevant variables when a deposit is successful.
+   * @notice Update the relevant variables when a deposit is successful.
    */
   function _updateDepositValues(address _address, uint256 _amount) private {
     s_vault[_address] += _amount;
@@ -161,7 +164,7 @@ contract KipuBank is ReentrancyGuard {
   }
 
   /**
-   * Update the relevant variables when a withdrawal is successful.
+   * @notice Update the relevant variables when a withdrawal is successful.
    */
   function _updateWithdrawValues(address _address, uint256 _amount) private {
     s_vault[_address] -= _amount;
